f803ffd141b21f69268dc9a9cb16da1e4306ef76
Carlos Martin <carlos.augusto.martin@gmail.com>
2025-08-11 15:18:06 -0300

    V1.1 todos los endpoints funcionando con Response Entity

--- src/main/java/com/camartin/forohubapi/forohub/controller/TopicoController.java
+++ src/main/java/com/camartin/forohubapi/forohub/controller/TopicoController.java
@@ -1,13 +1,12 @@
 package com.camartin.forohubapi.forohub.controller;
 
-import com.camartin.forohubapi.forohub.topico.DatosActualizacionTopico;
-import com.camartin.forohubapi.forohub.topico.DatosCrearTopico;
-import com.camartin.forohubapi.forohub.topico.TopicoRepository;
-import com.camartin.forohubapi.forohub.topico.Topico;
+import com.camartin.forohubapi.forohub.topico.*;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,31 +20,38 @@ public class TopicoController {
 
     @Transactional
     @PostMapping
-    public void crear(@RequestBody @Valid DatosCrearTopico topico) {
-        repository.save(new Topico(topico));
+    public ResponseEntity crear(@RequestBody @Valid DatosCrearTopico datos, UriComponentsBuilder uriComponentsBuilder) {
+        var topico = new Topico(datos);
+        repository.save(topico);
+        var uri = uriComponentsBuilder.path("/topicos/{id}").buildAndExpand(topico. getId()).toUri();
+        return ResponseEntity.created(uri).body(new DatosDetalleTopico(topico));
     }
 
     @GetMapping
-    public List<Topico> listar() {
-        return repository.findAll();
+    public ResponseEntity listar() {
+        var respuesta = repository.findAll();
+        return ResponseEntity.ok(respuesta);
     }
 
     @GetMapping("/{id}")
-    public Optional<Topico> listarPorId(@PathVariable Long id) {
-        return repository.findById(id);
+    public ResponseEntity listarPorId(@PathVariable Long id) {
+        var topico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DatosDetalleTopico(topico));
     }
 
     @Transactional
     @PutMapping("/{id}")
-    public void actualizar(@RequestBody @Valid DatosActualizacionTopico datos, @PathVariable Long id){
+    public ResponseEntity actualizar(@RequestBody @Valid DatosActualizacionTopico datos, @PathVariable Long id){
         var topico = repository.getReferenceById(id);
         topico.actualizarDatos(datos);
+        return ResponseEntity.ok(new DatosDetalleTopico(topico));
     }
 
     @Transactional
     @DeleteMapping("/{id}")
-    public void borrar(@PathVariable Long id){
+    public ResponseEntity borrar(@PathVariable Long id){
         repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
---
+++ src/main/java/com/camartin/forohubapi/forohub/topico/DatosDetalleTopico.java
@@ -0,0 +1,27 @@
+package com.camartin.forohubapi.forohub.topico;
+
+import java.util.List;
+
+public record DatosDetalleTopico(
+        Long id,
+        String titulo,
+        String mensaje,
+        String fechaCreacion,
+        Status estado,
+        String autor,
+        Curso curso,
+        List<String> respuestas
+) {
+    public DatosDetalleTopico(Topico topico) {
+        this(
+                topico.getId(),
+                topico.getTitulo(),
+                topico.getMensaje(),
+                topico.getFechaCreacion(),
+                topico.getEstado(),
+                topico.getAutor(),
+                topico.getCurso(),
+                topico.getRespuestas()
+        );
+    }
+}
