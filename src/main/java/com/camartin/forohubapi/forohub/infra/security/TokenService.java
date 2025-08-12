98d786c9260e60c09c43aa66d1310e61f7e1a4c7
Carlos Martin <carlos.augusto.martin@gmail.com>
2025-08-12 11:18:49 -0300

    V1.4 aplicaciòn completa funcionando

--- src/main/java/com/camartin/forohubapi/forohub/infra/security/TokenService.java
+++ src/main/java/com/camartin/forohubapi/forohub/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.camartin.forohubapi.forohub.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.camartin.forohubapi.forohub.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -35,5 +36,18 @@ public class TokenService {
 
     private Instant fechaExpiracion() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    public String getSubject(String tokenJWT){
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("Foro Hub")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT inválido o expirado", exception);
+        }
     }
 }
